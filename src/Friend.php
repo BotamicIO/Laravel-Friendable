1c2cee1f1cbf25ec45ff5c8428eab23ce20d8e1c
Brian Faust <hello@brianfaust.de>
2016-12-04 09:23:27 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Friend.php
+++ src/Friend.php
@@ -23,11 +23,7 @@ declare(strict_types=1);
 namespace BrianFaust\Friendable;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Friend extends Model
 {
