cd3f657b7b93bfeb6fa90a5a3822d8f572f9399c
Brian Faust <faustbrian@users.noreply.github.com>
2016-12-04 11:23:32 +0200

    Apply fixes from StyleCI (#6)
    [ci skip] [skip ci]

--- src/HasFriends.php
+++ src/HasFriends.php
@@ -23,11 +23,6 @@ declare(strict_types=1);
 namespace BrianFaust\Friendable;
 
 use Illuminate\Database\Eloquent\Model;
-
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 trait HasFriends
