c4a4f34dff85f115a3290e5fcbb9cf86c81067dc
Brian Faust <faustbrian@users.noreply.github.com>
2017-03-20 08:48:52 +0200

    Fix service provider reference

--- src/FriendableServiceProvider.php
+++ src/FriendableServiceProvider.php
@@ -13,9 +13,9 @@ declare(strict_types=1);
 
 namespace BrianFaust\Friendable;
 
-use BrianFaust\ServiceProvider\ServiceProvider;
+use BrianFaust\ServiceProvider\AbstractServiceProvider;
 
-class FriendableServiceProvider extends ServiceProvider
+class FriendableServiceProvider extends AbstractServiceProvider
 {
     /**
      * Bootstrap the application services.
